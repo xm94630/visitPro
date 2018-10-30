93d8661356c8ca8e47276c7ad1db6aa19f502226
xm94630 <xm94630@126.com>
2018-10-30 08:15:57 +0800

    修复之前的大量bug

--- check.py
+++ check.py
@@ -8,7 +8,7 @@ import re
 # 用这个网页去验证，遇到不可用ip会抛异常
 url = "http://httpbin.org/ip"
 #xm:注意这个超时设置是这样子写的，单位是秒
-socket.setdefaulttimeout(10)
+socket.setdefaulttimeout(3)
 # 从命令行获取代理ip
 try:
     proxyIp =  sys.argv[1]
@@ -17,20 +17,24 @@ except:
 
 if proxyIp:
     print '【单独验证】'
-    proxy={"http":"http://"+proxyIp}
+    proxys=[{"http":"http://"+proxyIp}]
+    i=0
     try:
-        res = urllib.urlopen(url,proxies=proxy)
+        res = urllib.urlopen(url,proxies=proxys[i])
+        html = res.read()
+        print(html)
         if res.code==200:
-            html = res.read()
-            if re.match(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',html):
-                print html
+            # 如果html中包含"origin"，则成功（因为有的代理ip居然返回“有道”的html！）
+            arr = re.findall(r'"origin"',html)
+            if len(arr)>0:
+                print '--------------------------'
+                print "[xm]本ip可以使用"
             else:
-                #print html
-                print 'sb!!' 
+                print '[xm]搞笑吧！' 
         else:
-            print '[code]' + str(res.code)
+            print '[xm]返回码为' + str(res.code)
     except Exception,e:
-        print 'error!!'
+        print '[xm]请求发生问题！'
         print e
 
 else:
@@ -43,27 +47,32 @@ else:
         proxy_temp = {"http":proxy_host}
         proxys.append(proxy_temp)
 
-    # 将可用ip写入valid_ip.txt
+    # 将可用ip写入ip2.txt
     ouf = open("ip2.txt", "a+")
 
-    len = len(proxys)
-    for i in range(0,len):
+    #神坑千万别那么写了！len是关键词
+    #len = len(proxys)
+    length = len(proxys);
+
+    for i in range(0,length):
         myIp = proxys[i]['http'][7:]
-        print '---------------------------------------------------------------------------------('+str(i)+'/'+str(len)+')';
+        print '---------------------------------------------------------------------------------('+str(i)+'/'+str(length)+')';
         print myIp.strip() #xm:这个myIp后面有个空行，我把它去了
         try:
             res = urllib.urlopen(url,proxies=proxys[i])
+            html = res.read()
+            #print(html)
             if res.code==200:
-                html = res.read()
-                if re.match(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',html):
-                    print 'success!'
+                # 如果html中包含"origin"，则成功（因为有的代理ip居然返回“有道”的html！）
+                arr = re.findall(r'"origin"',html)
+                if len(arr)>0:
+                    print "[xm]本ip可以使用"
+                    print '--------------------------写入文件'
                     ouf.write(myIp)
                 else:
-                    #print html
-                    print 'sb!!' 
+                    print '[xm]搞笑吧！' 
             else:
-                print '[code]' + str(res.code)
-
+                print '[xm]返回码为' + str(res.code)
         except Exception,e:
+            print '[xm]请求发生问题！'
             print e
-            continue
